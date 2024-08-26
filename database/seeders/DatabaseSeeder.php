698c6fbf4147086be57da75ff0b437b9e483e950
Ilham Ghazali <m.ilhamghazali@gmail.com>
2024-08-26 13:39:56 +0700

    feat(seeders): Add roles and users seed data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +16,47 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // \App\Models\Role::factory()->create([
+        //     'name' => 'Admin',
+        //     'description' => 'Admin',
+        // ]);
+        //  \App\Models\Role::factory()->create([
+        //     'name' => 'Staff',
+        //     'description' => 'Staff',
+        // ]);
+        // \App\Models\Role::factory()->create([
+        //     'name' => 'User',
+        //     'description' => 'User',
+        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Test Ilham ',
+            'email' => 'ilham@admin.com',
+            // 'role_id' => 1,
+            'password' => Hash::make('12345678'),
+            'role' => 'admin',
         ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Test user ',
+            'email' => 'user@user.com',
+            // 'role_id' => 3,
+            'password' => Hash::make('45678912'),
+
+        ]);
+
+        //data dumy for company
+        \App\Models\Company::factory()->create([
+            'name' => 'Company 1',
+            'description' => 'Company 1',
+            'address' => 'Jl. Raya jalanjalan No. 1',
+            'phone' => '081234567890',
+            'email' => '3v5wL@example.com',
+            'latitude' => '',
+            'longitude' => '107.6299',
+            'radius_km' => '0.5',
+        ]);
+        //call seed
+        // $this->call([
+        //     CompanySeeder::class,
+        // ]);
     }
 }
